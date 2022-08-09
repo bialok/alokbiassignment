5fa3eee1f2835bd9e9d8f628c084b4def46a75c6
Alok Pandey <alok.pandey@binaryinformatics.com>
2022-08-09 15:49:02 +0530

    initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='header'>
+        <img src='https://binaryinformatics.co/assets/binary-icon%20(1).svg'
+        alt='' 
+        />
+        <div className='header-text'>
+            <div>
+                <p>Talk to an Expert Call Toll Free</p>
+                <p>+1509-619-7072</p>
+            </div>
+            <button className='header-button'>TRY IT FREE</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Nav;
