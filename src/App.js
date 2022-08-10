3c33722260b01717acd6d8b3518affe87348d85e
alokbinary <alok.pandey@binaryinformatics.com>
2022-08-10 11:18:44 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Nav from './components/Nav.js';
+import Banner from './components/Banner.js';
+
 function App() {
   return (
     <div className="App">
      <Nav/>
+     <Banner/>
+
     
     </div>
   );
